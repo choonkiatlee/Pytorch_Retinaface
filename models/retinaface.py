4cfa5e2eeb78c5df6873ad3d3df2dbc3409180e3
CK <choonkiat.lee@gmail.com>
2020-04-15 15:31:56 +0800

    update PriorBox

--- models/retinaface.py
+++ models/retinaface.py
@@ -12,7 +12,7 @@ from models.net import SSH as SSH
 from math import ceil
 from itertools import product as product
 
-
+from typing import *
 
 class ClassHead(nn.Module):
     def __init__(self,inchannels=512,num_anchors=3):
@@ -49,7 +49,12 @@ class LandmarkHead(nn.Module):
         return out.view(out.shape[0], -1, 10)
 
 class RetinaFace(nn.Module):
-    def __init__(self, cfg = None, phase = 'train', calculate_prior_boxes = False, backbone = None):
+    def __init__(self, 
+        cfg = None, 
+        phase = 'train', 
+        calculate_prior_boxes = False, 
+        backbone = None,
+        ):
         """
         :param cfg:  Network related settings.
         :param phase: train or test.
@@ -91,6 +96,136 @@ class RetinaFace(nn.Module):
         self.ClassHead = self._make_class_head(fpn_num=3, inchannels=cfg['out_channel'])
         self.BboxHead = self._make_bbox_head(fpn_num=3, inchannels=cfg['out_channel'])
         self.LandmarkHead = self._make_landmark_head(fpn_num=3, inchannels=cfg['out_channel'])
+
+    def _make_class_head(self,fpn_num=3,inchannels=64,anchor_num=2):
+        classhead = nn.ModuleList()
+        for i in range(fpn_num):
+            classhead.append(ClassHead(inchannels,anchor_num))
+        return classhead
+    
+    def _make_bbox_head(self,fpn_num=3,inchannels=64,anchor_num=2):
+        bboxhead = nn.ModuleList()
+        for i in range(fpn_num):
+            bboxhead.append(BboxHead(inchannels,anchor_num))
+        return bboxhead
+
+    def _make_landmark_head(self,fpn_num=3,inchannels=64,anchor_num=2):
+        landmarkhead = nn.ModuleList()
+        for i in range(fpn_num):
+            landmarkhead.append(LandmarkHead(inchannels,anchor_num))
+        return landmarkhead
+
+    # def _prior_box(self, image_size, cfg):
+
+    #     min_sizes_list = cfg['min_sizes']
+    #     steps = cfg['steps']
+    #     clip = cfg['clip']
+    #     name = "s"
+
+    #     feature_maps = [[ceil(image_size[0]/step), ceil(image_size[1]/step)] for step in steps]
+    #     anchors = []
+    #     for k, f in enumerate(feature_maps):
+    #         min_sizes = min_sizes_list[k]
+    #         for i, j in product(range(f[0]), range(f[1])):
+    #             for min_size in min_sizes:
+    #                 s_kx = min_size / image_size[1]
+    #                 s_ky = min_size / image_size[0]
+    #                 dense_cx = [x * steps[k] / image_size[1] for x in [j + 0.5]]
+    #                 dense_cy = [y * steps[k] / image_size[0] for y in [i + 0.5]]
+    #                 for cy, cx in product(dense_cy, dense_cx):
+    #                     anchors += [cx, cy, s_kx, s_ky]
+
+    #     # back to torch land
+    #     output = torch.Tensor(anchors).view(-1, 4)
+    #     if clip:
+    #         output.clamp_(max=1, min=0)
+    #     return output
+
+    def forward(self,inputs):
+        out = self.body(inputs)
+
+        # FPN
+        fpn = self.fpn(out)
+
+        # SSH
+        feature1 = self.ssh1(fpn[0])
+        feature2 = self.ssh2(fpn[1])
+        feature3 = self.ssh3(fpn[2])
+        features = [feature1, feature2, feature3]
+
+        bbox_regressions = torch.cat([self.BboxHead[i](feature) for i, feature in enumerate(features)], dim=1)
+        classifications = torch.cat([self.ClassHead[i](feature) for i, feature in enumerate(features)],dim=1)
+        ldm_regressions = torch.cat([self.LandmarkHead[i](feature) for i, feature in enumerate(features)], dim=1)
+
+        # if self.calculate_prior_boxes:
+        #     prior_boxes = self._prior_box((inputs.shape[1], inputs.shape[2]), self.cfg)
+        # else:
+        #     prior_boxes = None
+
+        # if self.phase == 'train':
+        #     output = (bbox_regressions, classifications, ldm_regressions, prior_boxes)
+        # else:
+        #     output = (bbox_regressions, F.softmax(classifications, dim=-1), ldm_regressions, prior_boxes)
+
+        if self.phase == 'train':
+            output = (bbox_regressions, classifications, ldm_regressions)
+        else:
+            output = (bbox_regressions, F.softmax(classifications, dim=-1), ldm_regressions)
+
+        return output
+
+
+class RetinaFaceModified(nn.Module):
+    def __init__(self, 
+        # cfg = None, 
+        phase = 'train', 
+        calculate_prior_boxes = False, 
+        backbone = None,
+        return_layers:Dict[str, int] = {'stage1': 1, 'stage2': 2, 'stage3': 3},
+        in_channel:int = 32,
+        out_channel:int = 64,
+        ):
+        """
+        :param cfg:  Network related settings.
+        :param phase: train or test.
+        """
+        super(RetinaFace,self).__init__()
+        self.phase = phase
+        # backbone = None
+        # if cfg['name'] == 'mobilenet0.25':
+        #     backbone = MobileNetV1()
+        #     if cfg['pretrain']:
+        #         checkpoint = torch.load("./weights/mobilenetV1X0.25_pretrain.tar", map_location=torch.device('cpu'))
+        #         from collections import OrderedDict
+        #         new_state_dict = OrderedDict()
+        #         for k, v in checkpoint['state_dict'].items():
+        #             name = k[7:]  # remove module.
+        #             new_state_dict[name] = v
+        #         # load params
+        #         backbone.load_state_dict(new_state_dict)
+        # elif cfg['name'] == 'Resnet50':
+        #     import torchvision.models as models
+        #     backbone = models.resnet50(pretrained=cfg['pretrain'])
+
+        self.cfg = cfg
+        self.calculate_prior_boxes = calculate_prior_boxes
+
+        self.body = _utils.IntermediateLayerGetter(backbone, return_layers)
+        in_channels_stage2 = in_channel
+        in_channels_list = [
+            in_channels_stage2 * 2,
+            in_channels_stage2 * 4,
+            in_channels_stage2 * 8,
+        ]
+        out_channels = out_channel
+        self.fpn = FPN(in_channels_list,out_channels)
+        self.ssh1 = SSH(out_channels, out_channels)
+        self.ssh2 = SSH(out_channels, out_channels)
+        self.ssh3 = SSH(out_channels, out_channels)
+
+        self.ClassHead = self._make_class_head(fpn_num=3, inchannels=out_channel)
+        self.BboxHead = self._make_bbox_head(fpn_num=3, inchannels=out_channel)
+        self.LandmarkHead = self._make_landmark_head(fpn_num=3, inchannels=out_channel)
 
     def _make_class_head(self,fpn_num=3,inchannels=64,anchor_num=2):
         classhead = nn.ModuleList()
